0d4fb810cda04a42f32281c47840dbe81577a60a
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-07-02 21:52:47 +0200

    header en place

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { LuPower } from 'react-icons/lu';
+import { LuPowerOff } from 'react-icons/lu';
+import { FaUserPlus } from 'react-icons/fa';
 export default function Header() {
   return (
     <div className='header'>
+    <div className="icones-header"></div>
+        <div className="spacer"></div>
       <h1>Header</h1>
+      <div className="spacer"></div>
+      <div className="icones-header">
+      <div className="icone-header LuPower"><LuPower/></div>
+      <div className="icone-header LuPowerOff"><LuPowerOff/></div>
+      <div className="icone-header FaUserPlus"><FaUserPlus/></div>
+      </div>
     </div>
   )
 }
