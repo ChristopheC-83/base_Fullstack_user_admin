987acbcd50f9c1f1d3e187a9360fc91de1dea25c
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-07-03 06:15:36 +0200

    prise de tete sur btn connexion

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import { LuPower } from 'react-icons/lu';
-import { LuPowerOff } from 'react-icons/lu';
-import { FaUserPlus } from 'react-icons/fa';
+import React from "react";
+import { LuPower } from "react-icons/lu";
+import { LuPowerOff } from "react-icons/lu";
+import { FaUserPlus } from "react-icons/fa";
+import { BsPencilFill } from "react-icons/bs";
 export default function Header() {
   return (
-    <div className='header'>
-    <div className="icones-header"></div>
-        <div className="spacer"></div>
+    <div className="header">
+      <div className="icone-header-container "></div>
+      <div className="spacer"></div>
       <h1>Header</h1>
       <div className="spacer"></div>
-      <div className="icones-header">
-      <div className="icone-header LuPower"><LuPower/></div>
-      <div className="icone-header LuPowerOff"><LuPowerOff/></div>
-      <div className="icone-header FaUserPlus"><FaUserPlus/></div>
+
+      <div className="icone-header-container connected dnone">
+        <div className="icone-header FaUserPlus">
+          <FaUserPlus />
+        </div>
+        <div className="icone-header LuPower">
+          <LuPower />
+        </div>
       </div>
+
+      <div className="icone-header-container disconnected">
+        <div className="icone-header BsPencilFill">
+          <BsPencilFill />
+        </div>
+        <div className="icone-header LuPowerOff">
+          <LuPowerOff />
+        </div>
+      </div>
+
+
     </div>
-  )
+  );
 }
